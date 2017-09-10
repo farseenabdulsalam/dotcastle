e3a9b8cd79483e9df856f24da2f56512a411d80b
Farzeen <farseenabdulsalam@gmail.com>
2017-09-11 01:30:01 +0530

    AppMgr.test: test list_config_groups_for_app

--- test/AppMgr.test.cpp
+++ test/AppMgr.test.cpp
@@ -45,7 +45,7 @@ struct fixture_invalid_dotcastle_dir : public fixture_base {
 };
 
 
-BOOST_FIXTURE_TEST_CASE (list_apps_normal, fixture_valid_dotcastle_dir) {
+BOOST_FIXTURE_TEST_CASE (list_apps, fixture_valid_dotcastle_dir) {
   // fs::path dotcastle_dir from fixture
   vector<App> created_apps;
   for(int i=0;i<3;i++) {
@@ -66,11 +66,46 @@ BOOST_FIXTURE_TEST_CASE (list_apps_normal, fixture_valid_dotcastle_dir) {
   }
 }
 
-BOOST_FIXTURE_TEST_CASE (list_apps_exception, fixture_invalid_dotcastle_dir) {
+BOOST_FIXTURE_TEST_CASE (list_apps_invalid_dotcastle_dir,
+                         fixture_invalid_dotcastle_dir) {
   // fs::path dotcastle_dir from fixture
   AppMgr app_mgr(dotcastle_dir.string());
   BOOST_CHECK_THROW(app_mgr.list_apps(),InvalidDotcastleDir);
 }
+
+BOOST_FIXTURE_TEST_CASE (list_config_groups_for_app_invalid_dotcastle_dir,
+                         fixture_invalid_dotcastle_dir) {
+  // fs::path dotcastle_dir from fixture
+  AppMgr app_mgr(dotcastle_dir.string());
+  BOOST_CHECK_THROW(app_mgr.list_config_groups_for_app({"doesnt matter"}),
+                    InvalidDotcastleDir);
+}
   
-    
+BOOST_FIXTURE_TEST_CASE (list_config_groups_for_app_non_existent_app,
+                         fixture_valid_dotcastle_dir) {
+  // fs::path dotcastle_dir from fixture
+  AppMgr app_mgr(dotcastle_dir.string());
+  BOOST_CHECK_THROW(app_mgr.list_config_groups_for_app({"doesnt matter"}),
+                    NonExistentApp);
+}
+
+BOOST_FIXTURE_TEST_CASE (list_config_groups_for_app,
+                         fixture_valid_dotcastle_dir) {
+  // fs::path dotcastle_dir from fixture
+  AppMgr app_mgr(dotcastle_dir.string());
+
+  string app_name("app_test");
+  auto app_dir_path = dotcastle_dir/app_name;
+  string config_group_name("config_group_test");
+  auto config_group_install_file = app_dir_path/(config_group_name+".install");
+app_dir_path/(config_group_name+".install");
+  fs::create_directory(app_dir_path);
+  fs::ofstream file_stream(config_group_install_file);
+  file_stream.close();
+
+  auto listed_config_groups = app_mgr.list_config_groups_for_app({app_name});
+  
+  BOOST_CHECK_EQUAL(listed_config_groups.front().name,config_group_name);
+
+}
 
