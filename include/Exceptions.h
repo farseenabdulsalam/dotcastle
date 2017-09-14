65810412e997f7dd84de7c6fba800927a3ed0b9e
* <Farzeen>
2017-09-14 17:37:36 +0530

    AppConfigGroupMakeFailed exception defined

--- include/Exceptions.h
+++ include/Exceptions.h
@@ -1,5 +1,9 @@
 
 class DotcastleError {};
 
+// TODO: add given_path to the exception class
 class InvalidDotcastleDir: public DotcastleError {};
+// TODO: add name of app to the exception class
 class NonExistentApp: public DotcastleError {};
+// TODO: add retcode, app name, config group name to the exception class
+class AppConfigGroupMakeFailed: public DotcastleError {};
