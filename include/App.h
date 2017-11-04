6afd9d86b275cf0c63fda4b42c75909b01ea2d54
Farzeen <farseenabdulsalam@gmail.com>
2017-11-04 23:22:24 +0530

    Implemented operator== for App and ConfigGroup.

--- include/App.h
+++ include/App.h
@@ -1,7 +1,8 @@
 #include <string>
 using namespace std;
 struct App {
-  
   string name;
-  
+  bool operator==(const App &app) {
+    return name == app.name;
+  }
 };
--- include/ConfigGroup.h
+++ include/ConfigGroup.h
@@ -1,8 +1,9 @@
 #include <string>
 
 struct ConfigGroup {
-
   string name;
-
+  bool operator== (const ConfigGroup &config_group) {
+    return name == config_group.name;
+  }
 };
 
